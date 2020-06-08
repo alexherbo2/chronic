a35fce4ddbccbbacb0f63d22a38ab0cbb88bc79f
Alex Leferry 2 <alexherbo2@gmail.com>
2020-06-08 09:52:41 +0200

    Add support for exclusive time range

--- src/chronic/range.cr
+++ src/chronic/range.cr
@@ -5,22 +5,26 @@ class Chronic::Range
   property step : ::Time::Span
 
   def self.new(range : ::Range(::Time, ::Time))
-    new(range.begin, range.end)
+    new(range.begin, range.end, range.exclusive?)
   end
 
-  def initialize(@begin : ::Time, @end : ::Time)
+  def initialize(@begin : ::Time, @end : ::Time, @exclusive = false)
     @time = @begin
     @step = 1.second
   end
 
   def next
-    if @time <= @end
+    if (@exclusive == true && @time < @end) || (@exclusive == false && @time <= @end)
       current_time = @time
       @time += @step
       current_time
     else
       stop
     end
+  end
+
+  def exclusive?
+    @exclusive
   end
 
 end
